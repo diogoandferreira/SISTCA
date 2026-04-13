a2046dc8832d5fcc59255f7efeb66d6d7869667e
diogo_ferreira <ferreira.and.diogo@gmail.com>
2026-04-13 11:29:55 +0100

    Python code

---
+++ main.py
@@ -0,0 +1,77 @@
+import network, time, dht, ujson 
+
+from machine import Pin 
+
+from umqtt.simple import MQTTClient 
+
+  
+
+HOST  = "mqtt.thingsboard.cloud" 
+
+PORT  = 1883 
+
+TOKEN = "YOUR_ACCESS_TOKEN_HERE" 
+
+TOPIC = b"v1/devices/me/telemetry" 
+
+  
+
+sensor = dht.DHT22(Pin(15)) 	#if you choose another GPIO pin, change his number here
+
+  
+
+# Connect to Wi-Fi 
+
+sta = network.WLAN(network.STA_IF) 
+
+sta.active(True) 
+
+sta.connect("Wokwi-GUEST", "") 
+
+while not sta.isconnected(): 
+
+    time.sleep(0.5) 
+
+print("WiFi OK!") 
+
+  
+
+# Connect via MQTT 
+
+client = MQTTClient( 
+
+    client_id="esp32_tb", 
+
+    server=HOST, port=PORT, 
+
+    user=TOKEN, password="", 
+
+    keepalive=60 
+
+) 
+
+client.connect() 
+
+print("MQTT connected!") 
+
+  
+
+# Main loop — publish every 10 seconds 
+
+while True: 
+
+    sensor.measure() 
+
+    payload = ujson.dumps({ 
+
+        "temperature": sensor.temperature(), 
+
+        "humidity":    sensor.humidity() 
+
+    }) 
+
+    client.publish(TOPIC, payload) 
+
+    print("Sent:", payload) 
+
+    time.sleep(3) 
